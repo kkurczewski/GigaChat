a22524cbe2982360662a57acd41740c3d90fa580
kurczews <krzysiek.kurczewski@gmail.com>
2022-08-06 11:00:31 +0200

    Clear changes when extension disabled

--- src/overlay.js
+++ src/overlay.js
@@ -1,8 +1,4 @@
 async function styleChatOverlay(parent, options, fullscreenEnabled) {
-  if (!options.enabled) {
-    // TODO auto rollback changes
-    return;
-  }
   console.info("Overlay enabled with options:", options);
 
   const videoContainer = parent.querySelector("#ytd-player #container");
@@ -13,22 +9,36 @@ async function styleChatOverlay(parent, options, fullscreenEnabled) {
 
   // critical section
   // moving node reloads frame (and clears style if already set)
-  if (liveChat.parentNode !== videoContainer) {
-    videoContainer.appendChild(liveChat);
-  }
+  positionChatContainer()
 
   // ...wait for iframe to reload
   const liveChatBox = await queryIFrameNode(liveChatFrame, "yt-live-chat-renderer");
 
   // ...before applying style
-  setChatOpacity(liveChatBox, options.opacity);
+  setChatOpacity(liveChatBox);
   styleScrollbar();
-  hideHeader(options.header);
-  hideToggleButton(options.toggleButton);
+  hideHeader();
+  hideToggleButton();
 
   console.info("Overlay applied");
 
+  async function positionChatContainer() {
+    if (!options.enabled) {
+      const chatSibling = parent.querySelector("#chat-template");
+      const originalParent = chatSibling.parentNode;
+      if (liveChat.parentNode !== originalParent) {
+        originalParent.insertBefore(liveChat, chatSibling);
+      }
+    } else if (liveChat.parentNode !== videoContainer) {
+      videoContainer.appendChild(liveChat);
+    }
+  }
+
   function setOverlayStyle(node) {
+    if (!options.enabled) {
+      node.style = "";
+      return;
+    }
     const overlayStyle = {
       position: "absolute",
       zIndex: 10,
@@ -65,7 +75,7 @@ async function styleChatOverlay(parent, options, fullscreenEnabled) {
 
   async function styleScrollbar() {
      const itemScroll = await queryIFrameNode(liveChatFrame, "#item-scroller");
-     if (options.position === "left") {
+     if (options.enabled && options.position === "left") {
        // show scrollbar always on edge of screen
        itemScroll.style.direction = "rtl";
        // ...but keep chat as usual
@@ -75,21 +85,32 @@ async function styleChatOverlay(parent, options, fullscreenEnabled) {
      }
   }
 
-  async function hideHeader(enabled) {
+  async function hideHeader() {
     const header = await queryIFrameNode(liveChatFrame, "yt-live-chat-header-renderer");
+    if (!options.enabled) {
+      header.style.opacity = 1.0;
+      header.style.display = "";
+      return;
+    }
     header.style.opacity = Math.min(options.opacity + 0.1, 1.0);
-    if (!enabled) {
+    if (!options.header) {
       header.style.display = "none";
     } else {
       header.style.display = "";
     }
   }
 
-  async function hideToggleButton(enabled) {
+  async function hideToggleButton() {
     const toggleButton = document.querySelector("#show-hide-button");
     const separator = await queryIFrameNode(liveChatFrame, "#input-panel");
+    if (!options.enabled) {
+      toggleButton.style.opacity = 1.0;
+      toggleButton.style.display = "";
+      separator.style.display = "";
+      return;
+    }
     toggleButton.style.opacity = Math.min(options.opacity + 0.1, 1.0);
-    if (!enabled) {
+    if (options.enabled && !options.toggleButton) {
       toggleButton.style.display = "none";
       separator.style.display = "none";
     } else {
@@ -98,11 +119,11 @@ async function styleChatOverlay(parent, options, fullscreenEnabled) {
     }
   }
 
-  function setChatOpacity(node, opacity) {
+  function setChatOpacity(node) {
     const backgroundColor = window.getComputedStyle(node).backgroundColor;
-    node.style.background = addTransparency(backgroundColor);
+    node.style.background = addTransparency(backgroundColor, options.enabled ? options.opacity : 1.0);
 
-    function addTransparency(rgbColor) {
+    function addTransparency(rgbColor, opacity) {
       // color returned from style property is string of format "rgb(xx,yy,zz)"
       if (rgbColor.startsWith('rgba')) {
         return rgbColor.replace(/[0-9]+[)]/, opacity + ')')
