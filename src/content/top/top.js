34d57f7112762bc08d2b7c7f7438b0d423d80f2b
Krzysztof Kurczewski <krzysiek.kurczewski@gmail.com>
2024-11-09 13:17:57 +0100

    Apply selectors directly on chat container

--- src/content/top/top.js
+++ src/content/top/top.js
@@ -3,35 +3,33 @@
 window.addEventListener("load", async () => {
   console.debug("Lookup nodes...")
 
-  const root = await find(document, "#content #page-manager")
+  const root = await find(document, "#page-manager")
   console.debug("Found", root)
 
   const videoContainer = await find(root, "ytd-watch-flexy")
   console.debug("Found", videoContainer)
 
+  const chatContainer = await find(videoContainer, "#chat-container")
+  console.debug("Found", chatContainer)
+
   options.enabled(enabled => {
     window.performance.mark("enabled-changed")
-    videoContainer.classList.toggle("overlay", enabled)
+    chatContainer.classList.toggle("overlay", enabled)
   })
   options.position(position => {
     window.performance.mark("position-changed")
-    videoContainer.classList.toggle("left", position === "left")
+    chatContainer.classList.toggle("left", position === "left")
   })
   options.settings(settings => {
     window.performance.mark("settings-changed")
-    videoContainer.dataset.settings = settings
+    chatContainer.dataset.settings = settings
   })
-
-  observe(videoContainer, "#chat", async (chat) => {
-    console.debug("Found", chat)
-
-    options.topMargin(topMargin => {
-      window.performance.mark("top-margin-changed")
-      chat.style.setProperty("--top-margin", `${topMargin}%`)
-    })
-    options.bottomMargin(bottomMargin => {
-      window.performance.mark("bottom-margin-changed")
-      chat.style.setProperty("--bottom-margin", `${bottomMargin}%`)
-    })
+  options.topMargin(topMargin => {
+    window.performance.mark("top-margin-changed")
+    chatContainer.style.setProperty("--top-margin", `${topMargin}%`)
+  })
+  options.bottomMargin(bottomMargin => {
+    window.performance.mark("bottom-margin-changed")
+    chatContainer.style.setProperty("--bottom-margin", `${bottomMargin}%`)
   })
 })
